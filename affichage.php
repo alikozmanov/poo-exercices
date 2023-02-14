b950dad1103d6c4478aa86e607d48c6d7e3c5ef2
Alik Ozmanov <alik.ozmanov@hotmail.com>
2023-02-14 11:43:07 +0100

    commit

--- affichage.php
+++ affichage.php
@@ -16,5 +16,16 @@ class Vehicule {
     $mon_Vehicule->poids += 70;
     echo "le nouveau poids du vehicule est de ". $mon_Vehicule->poids . "kg";
 
+
+  
+  $poids_voiture = 1400; // poids initial de la voiture en kg
+  $poids_personne = 65; // poids de chaque personne en kg
+  $nb_personnes = 2; // nombre de personnes dans la voiture
+  $couleur_voiture = "verte"; // couleur de la voiture
+
+  $poids_total = $poids_voiture + ($poids_personne * $nb_personnes); // calcul du nouveau poids total
+  echo "La voiture $couleur_voiture pèse maintenant $poids_total kg."; // affichage du résultat
+
+
 }
 ?>
