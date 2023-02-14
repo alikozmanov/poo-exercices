f83b048f2ddf0f3158bd7bb3bd26890671f20c71
Alik Ozmanov <alik.ozmanov@hotmail.com>
2023-02-14 11:25:26 +0100

    commit

--- Vehicule.php
+++ Vehicule.php
@@ -12,7 +12,7 @@ class Vehicule {
         $this->poids = $poids;
 
     }
-    
+
     // méthode publique
     public function rouler() { 
         return "Le véhicule roule";
@@ -22,6 +22,36 @@ class Vehicule {
     public function ajouter_personne($poids_personne) { 
         $this->poids += $poids_personne; 
     } 
+
+
+    public function repeindre($couleur) {
+        $this->couleur = $couleur;
+
+    }
+    public function getCouleur() {
+        return $this->couleur;
+    }
+
+
+    public function mettre_essence($nombre_litre) {
+        $this->poids += $nombre_litre;
+    }
+    public function getPoids() {
+        return $this->poids;
+    }
+
+
+    public function ajouter_pneu_neige($nombre) {
+        $this->nombre_pneu_neige += $nombre;
+      }
+    
+    public function enlever_pneu_neige($nombre) {
+        $this->nombre_pneu_neige -= $nombre;
+      }
+    
+    public function getNombrePneuNeige() {
+        return $this->nombre_pneu_neige;
+      }
 }
 
 ?>
